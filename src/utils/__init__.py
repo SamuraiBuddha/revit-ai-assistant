e3c7b0dfd7bf61d72788bc5ea024fc86e719dc1b
Jordan Paul Ehrig <76662139+SamuraiBuddha@users.noreply.github.com>
2025-06-26 04:00:47 +0000

    Fix utils init - remove unimplemented import

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,5 @@
 """Utility functions for Revit AI Assistant"""
 
 from .agent_registry import AgentRegistry
-from .revit_api_wrapper import RevitAPIWrapper
 
-__all__ = ['AgentRegistry', 'RevitAPIWrapper']
+__all__ = ['AgentRegistry']
